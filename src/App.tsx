45bec077ed72e3a5946f90d8d7891706b0b1610a
Choalstn <choalstn@gmail.com>
2023-07-20 19:27:38 +0900

    [Design] : 전체 css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import CreateForm from './Pages/CreateForm';
+import Result from './Pages/Result';
+import Preview from './Pages/Preview';
+import Header from './Components/Header';
+
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<Header />
+			<div className="body">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<CreateForm />} />
+						<Route path="preview" element={<Preview />} />
+						<Route path="/result" element={<Result />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</>
+	);
 }
 
 export default App;
