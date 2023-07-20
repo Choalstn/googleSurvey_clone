b55c0346fcb692dfea2fd8e4d0bc5bc2a2840da1
Choalstn <choalstn@gmail.com>
2023-07-21 02:02:26 +0900

    [Feat] : 카드 타입 변경 리듀서 생성

--- src/store/cardSlice.tsx
+++ src/store/cardSlice.tsx
@@ -3,8 +3,19 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 export interface InterCard {
 	id: number;
 	title: string;
-	contents: string;
+	contents: string | OptionType[];
 	isFocused: boolean;
+	cardType: string;
+}
+
+interface OptionType {
+	id: string;
+	text?: string;
+}
+
+interface changeType {
+	id: number;
+	cardType: string;
 }
 
 const initialState: InterCard[] = [
@@ -13,6 +24,7 @@ const initialState: InterCard[] = [
 		title: '제목 없는 설문지',
 		contents: '',
 		isFocused: false,
+		cardType: '제목',
 	},
 ];
 
@@ -23,8 +35,45 @@ const cardSlice = createSlice({
 		addCard: (state: InterCard[], action: PayloadAction<InterCard>) => {
 			return [...state, action.payload];
 		},
+
+		changeType: (state: InterCard[], action: PayloadAction<changeType>) => {
+			const target = state.find(
+				(card) => card.id === action.payload.id,
+			) as InterCard;
+
+			if (
+				!(
+					target.cardType === '라디오' ||
+					target.cardType === '체크박스' ||
+					target.cardType === '드롭다운'
+				) &&
+				(action.payload.cardType === '라디오' ||
+					action.payload.cardType === '체크박스' ||
+					action.payload.cardType === '드롭다운')
+			) {
+				target.contents = [
+					{
+						id: String(Date.now()),
+						text: '옵션 1',
+					},
+				];
+			} else if (
+				(target.cardType === '라디오' ||
+					target.cardType === '체크박스' ||
+					target.cardType === '드롭다운') &&
+				!(
+					action.payload.cardType === '라디오' ||
+					action.payload.cardType === '체크박스' ||
+					action.payload.cardType === '드롭다운'
+				)
+			) {
+				target.contents = '';
+			}
+
+			target.cardType = action.payload.cardType as string;
+		},
 	},
 });
 
-export const { addCard } = cardSlice.actions;
+export const { addCard, changeType } = cardSlice.actions;
 export default cardSlice.reducer;
