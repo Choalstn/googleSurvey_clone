26398c7321510b2d3a840afbbb464cb03cbffc7c
Choalstn <choalstn@gmail.com>
2023-07-22 13:32:59 +0900

    [Feat] : 카드 복사 시 cardType 복제 안 되는 이슈 해결

--- src/Components/DropDown.tsx
+++ src/Components/DropDown.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import { Select as MSelect, SelectChangeEvent } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { changeType } from '../store/cardSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { InterCard, changeType } from '../store/cardSlice';
+import { RootState } from '../store';
 
 const Select = styled(MSelect)`
 	width: 45%;
@@ -20,6 +21,9 @@ interface Props {
 function DropDown({ id }: Props) {
 	const dispatch = useDispatch();
 	const arr = ['단답형', '장문형', '객관식 질문', '체크박스', '드롭다운'];
+	const cardInfo = useSelector((state: RootState) =>
+		state.cards.find((el) => el.id === id),
+	) as InterCard;
 
 	const handleSelect = (e: SelectChangeEvent<unknown>) => {
 		dispatch(changeType({ id, cardType: e.target.value as string }));
@@ -27,7 +31,11 @@ function DropDown({ id }: Props) {
 
 	return (
 		<>
-			<Select onChange={handleSelect} defaultValue="단답형">
+			<Select
+				onChange={handleSelect}
+				defaultValue="단답형"
+				value={cardInfo.cardType}
+			>
 				{arr.map((el, idx) => (
 					<MenuItem key={idx} value={el}>
 						{el}
