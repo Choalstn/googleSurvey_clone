cc5f89b6f547f6725cd5c412eb8998643100862d
Choalstn <choalstn@gmail.com>
2023-07-24 20:22:01 +0900

    [Feat] : redux-persist 적용

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+// store/index.tsx
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import cardSlice from './cardSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+const reducers = combineReducers({
+	cards: cardSlice,
+});
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	keyPrefix: '',
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-	reducer: {
-		cards: cardSlice,
-	},
+	reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
