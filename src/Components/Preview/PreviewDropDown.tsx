38cde1e058f6fdee3ff5d0197e6a8fc2fa367174
Choalstn <choalstn@gmail.com>
2023-07-25 01:03:45 +0900

    [Feat] : preview/카드 타입 드롭다운 컴포넌트 구현

--- src/Components/Preview/PreviewDropDown.tsx
+++ src/Components/Preview/PreviewDropDown.tsx
@@ -1,11 +1,45 @@
-import { InterCard } from '../../store/cardSlice';
+import styled from 'styled-components';
+import { InterCard, OptionType } from '../../store/cardSlice';
+import { Select as MSelect, SelectChangeEvent } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import { useEffect, useState } from 'react';
 
 interface Props {
 	cardInfo: InterCard;
 }
 
+const Select = styled(MSelect)`
+	margin-left: 22px;
+	margin-bottom: 20px;
+	width: 30%;
+	height: 48px;
+	fieldset {
+		border-width: 1px !important;
+	}
+`;
+
 function PreviewDropDown({ cardInfo }: Props) {
-	return <></>;
+	const [selectedOption, setSelectedOption] = useState('');
+
+	const contents = cardInfo.contents as OptionType[];
+	const options = [] as string[];
+	contents.map((el) => options.push(el.text!));
+	console.log(options);
+
+	const handleOptionChange = (e: SelectChangeEvent<unknown>) => {
+		setSelectedOption(e.target.value as string);
+	};
+	return (
+		<>
+			<Select value={selectedOption} onChange={(e) => handleOptionChange(e)}>
+				{options.map((el, idx) => (
+					<MenuItem key={idx} value={el}>
+						{el}
+					</MenuItem>
+				))}
+			</Select>
+		</>
+	);
 }
 
 export default PreviewDropDown;
