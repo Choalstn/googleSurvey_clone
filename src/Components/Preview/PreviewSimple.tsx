0604e7a623010cd23e2fd34b1157fad022ec8f64
Choalstn <choalstn@gmail.com>
2023-07-25 00:37:39 +0900

    [Feat] : preview/카드 타입 단답,장문 컴포넌트 구현

--- src/Components/Preview/PreviewSimple.tsx
+++ src/Components/Preview/PreviewSimple.tsx
@@ -1,11 +1,47 @@
+import styled, { css } from 'styled-components';
 import { InterCard } from '../../store/cardSlice';
 
 interface Props {
 	cardInfo: InterCard;
 }
 
+interface CardType {
+	cardType: string;
+}
+
+const Answer = styled.input<CardType>`
+	border: none;
+	margin: 15px;
+	height: 3vh;
+	width: 30%;
+	margin-left: 23px;
+	margin-bottom: 25px;
+	font-size: 15px;
+	border-bottom: 1px solid rgb(198, 199, 211);
+
+	${(props) =>
+		props.cardType === '장문형' &&
+		css`
+			width: 90%;
+		`}
+
+	&:hover {
+		cursor: pointer;
+	}
+
+	&:focus {
+		outline: none;
+		border-bottom: 3px solid #673ab6;
+	}
+`;
+
 function PreviewSimple({ cardInfo }: Props) {
-	return <></>;
+	console.log(cardInfo.cardType);
+	return (
+		<>
+			<Answer type="text" value="답변 ..." cardType={cardInfo.cardType} />
+		</>
+	);
 }
 
 export default PreviewSimple;
